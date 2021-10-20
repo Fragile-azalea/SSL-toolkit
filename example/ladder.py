3f25fe9985f131200b2c9c371d8da1b6437cdc7e
kp600168 <1872891695@qq.com>
2021-10-20 21:56:12 +0800

    remove old ladder

--- example/ladder.py
+++ example/ladder.py
@@ -1,59 +1,57 @@
-from allinone import SEMI_TRAINER_REGISTRY
-from allinone.data import SEMI_MNIST
-import hydra
-import logging
-from torch.nn import functional as F
-from homura.vision import MODEL_REGISTRY
-from homura.reporters import TQDMReporter, TensorboardReporter
-from homura.optim import Adam
-from managpu import GpuManager
-from homura.lr_scheduler import LambdaLR
-GpuManager().set_by_memory(1)
+# -*- coding: UTF-8 -*-
+import argparse
+from torch.optim import Adam
+from torch.optim.lr_scheduler import LambdaLR
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.plugins import DDPPlugin
+from DeSSL import MODEL_REGISTRY, SEMI_DATASET_REGISTRY
+from DeSSL.trainer import Ladder
 
 
-logger = logging.getLogger(__name__)
-
-
-@hydra.main(config_path="config", config_name='ladderv2.yml')
 def main(args):
-    logger.info(args)
-    mnist = SEMI_MNIST[args.dataset](args.root)
+    mnist = SEMI_DATASET_REGISTRY(args.dataset)(args.root, 10)
+    train_loader, test_loader, num_classes = mnist(
+        args.batch_size, num_workers=args.num_workers)
+    optimizer = {'optimizer': Adam, 'lr': args.lr_256 * args.batch_size / 256}
+    lr_scheduler = {'lr_scheduler': LambdaLR, 'lr_lambda': lambda epoch: epoch *
+                    0.18 + 0.1 if epoch < 5 else (1. if epoch < 50 else 1.5 - epoch / 100)}
     num_neurons = [1000, 500, 250, 250, 250, 10, ]
     sigma_noise = [0.3, 0.3, 0.3, 0.3, 0.3, 0.3, ]
     kwargs = {'num_neurons': num_neurons,
-              'sigma_noise': sigma_noise, 'input_sigma_noise': 0.3}
-    # sigma_noise = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, ]
-    # kwargs = {'num_neurons': num_neurons,
-    #           'sigma_noise': sigma_noise, 'input_sigma_noise': 0.0}
-    logger.info(kwargs)
-    train_loader, test_loader, num_classes = mnist(
-        args.batch_size, num_workers=args.num_workers, return_num_classes=True, pin_memory=False)
+              'sigma_noise': sigma_noise,
+              'input_sigma_noise': 0.3}
     lenet = MODEL_REGISTRY(args.model)((28, 28), **kwargs)
     lam_list = [1000., 10., 0.1, 0.1, 0.1, 0.1, 0.1]
-    # lam_list = [0.] * 7
-    lr_scheduler = LambdaLR(lambda epoch: epoch * 0.18 + 0.1 if epoch < 5 else (1. if epoch <
-                                                                                50 else 1.5 - epoch / 100))
-    kwargs = {'lam_list': lam_list, 'scheduler': lr_scheduler}
-    logger.info(kwargs)
-    trainer = SEMI_TRAINER_REGISTRY('Ladder')(
-        lenet,
-        Adam(
-            lr=args.lr_256 *
-            args.batch_size /
-            256
-        ),
-        F.nll_loss,
-        **kwargs,
-        reporters=[
-            TQDMReporter()])
 
-    for _ in trainer.epoch_range(args.epochs):
-        trainer.train(train_loader)
-        trainer.test(test_loader)
-        trainer.scheduler.step()
+    ladder = Ladder((train_loader, test_loader), optimizer,
+                    lr_scheduler, lenet, lam_list)
+    callbacks = [ModelCheckpoint(monitor="val_acc1",
+                                 filename="{val_acc1:.2f}",
+                                 save_weights_only=True,
+                                 mode='max'), ]
 
-    logger.info(f"Max Accuracy={max(trainer.history['accuracy/test'])}")
+    plugins = DDPPlugin(
+        find_unused_parameters=False) if args.accelerator == 'ddp' else None
+
+    trainer = pl.Trainer.from_argparse_args(
+        args,
+        callbacks=callbacks,
+        plugins=plugins)
+
+    trainer.fit(ladder)
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description="Working on Ladder")
+    parser = pl.Trainer.add_argparse_args(parser)
+    parser.add_argument("--root", type=str, default='/hdd1/public_data/')
+    parser.add_argument("--batch_size", type=int, default=100)
+    parser.add_argument("--model", type=str, default="Ladder_MLP")
+    parser.add_argument("--dataset", type=str, default="semi_mnist")
+    parser.add_argument("--num_workers", type=int, default=0)
+    parser.add_argument("--lr_256", type=float, default=0.0512)
+    parser.set_defaults(profiler="simple", max_epochs=150)
+    args = parser.parse_args()
+    print(args)
+    main(args)
